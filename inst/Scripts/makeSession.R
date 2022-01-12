340ffa775a8e3276ede1f3facc43b533acb17301
Duncan Temple Lang <duncan@r-project.org>
2022-01-12 10:15:10 -0800

    load the library

--- inst/Scripts/makeSession.R
+++ inst/Scripts/makeSession.R
@@ -1,10 +1,8 @@
-source("~/Classes/Davis/Tools/extractRCode.R")
+library(RSessionScript)
 input = commandArgs(TRUE)
 
 if(length(input) == 1)
   input[2] = sprintf("%s.Rdb", input[1])
-
-#print(input)
 
 k = getCode(input[1], output = TRUE)
 db = asRdbTranscript(k)
